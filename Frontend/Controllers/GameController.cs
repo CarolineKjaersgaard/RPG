f316adc767ee04a015b0a50954b42d1100aa3386
Thor271998 <thor.petersen@specialisterne.dk>
2025-11-26 02:11:33 -0800

    Begun work in frontend

---
+++ Frontend/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult IntroScreen()
+        {
+            return View();
+        }
+    }
+}
--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Game}/{action=IntroScreen}/{id?}")
     .WithStaticAssets();
 
 
