cc63823695d690f1f2e75308386d1e4aacb934b3
Thor271998 <thor.petersen@specialisterne.dk>
2025-11-27 01:26:41 -0800

    Prepared for introscreen api call

--- Frontend/Controllers/GameController.cs
+++ Frontend/Controllers/GameController.cs
@@ -4,9 +4,23 @@ namespace Frontend.Controllers
 {
     public class GameController : Controller
     {
+        private readonly IGameAPI _api;
+
+        public GameController(IGameAPI api)
+        {
+            _api = api;
+        }
+
         public IActionResult IntroScreen()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Start()
+        {
+            var result = _api.StartGame();
+            return result;
         }
     }
 }
