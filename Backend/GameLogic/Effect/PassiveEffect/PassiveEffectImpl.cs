d9b4912f6cdea14d735ff203ea317845e32c18e8
ewrefads <clara.donovan@outlook.com>
2025-11-27 11:54:13 +0100

    Added initial Implementation

---
+++ Backend/GameLogic/Effect/PassiveEffect/PassiveEffectImpl.cs
@@ -0,0 +1,27 @@
+using Backend.GameLogic.Entity;
+
+namespace Backend.GameLogic.Effect.PassiveEffect
+{
+    public class PassiveEffect : IPassiveEffect
+    {
+        public bool ApplyEffect(IEntity target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPassive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveEffect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
