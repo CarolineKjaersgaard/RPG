18dc7bf13c9745902a3f1bd62fefd7d8acccc504
ewrefads <clara.donovan@outlook.com>
2025-11-27 14:33:01 +0100

    Began implementing IRoom

--- Backend/GameLogic/Room/Room.cs
+++ Backend/GameLogic/Room/Room.cs
@@ -1,3 +1,5 @@
+using System.Collections.Specialized;
+using Backend.GameLogic.Effect;
 using Backend.GameLogic.Entity;
 using Backend.GameLogic.Item;
 
@@ -5,19 +7,38 @@ namespace Backend.GameLogic
 {
     public class RoomImpl : IRoom
     {
+        string title;
+        bool isUnlocked;
+        string desc;
+        /// <summary>
+        /// 0 = no door
+        /// 1 = closed door
+        /// 2 = open door
+        /// </summary>
+        Dictionary<(int, int), int> doors = new Dictionary<(int, int), int>();
+        (int, int) entryDoor = (0,0);
+        IEffect effect;
+        string image;
+        List<IEntity> monsters = new List<IEntity>();
+        List<IItem> items = new List<IItem>();
+        (int, int) coords;
         public List<IItem> GetItems()
         {
-            throw new NotImplementedException();
+            return items;
         }
 
         public List<IEntity> GetMonsters()
         {
-            return new List<IEntity>();
+            return monsters;
         }
 
         public void SetEntryDoor((int, int) playerCoords)
         {
-            throw new NotImplementedException();
+            (int, int) index = (playerCoords.Item1 - coords.Item1, playerCoords.Item2 - coords.Item2);
+            if(doors.ContainsKey(index))
+            {
+                
+            }
         }
     }
 }
