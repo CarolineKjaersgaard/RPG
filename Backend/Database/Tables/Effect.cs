a07f3514b56db239c70a3731ee79f362dc29cc79
CarolineKjaersgaard <caro.thorslund@gmail.com>
2025-11-27 13:15:12 +0100

    Classes made

--- Backend/Database/Tables/Effect.cs
+++ Backend/Database/Tables/Effect.cs
@@ -2,18 +2,11 @@ using System;
 using System.Data;
 
 namespace Backend.Database.Tables {
-    public class Room : ITable
+    public class Effect : ITable
 {
     public string Id { get; set; }
     public string Title { get; set; }
     public string Description {get; set;}
-    public int Doors {get; set;}
-    public int Rarity { get; set; }
-    public int Difficulty {get; set;}
-    public string EffectId {get; set;}
-    public Effect Effect {get; set;}
-    public List<string> TagIds {get; set;}
-    public ICollection<Tag> Tags {get; set;}
 
 
     public List<string> GetCollumns()
--- Backend/Database/Tables/Tag.cs
+++ Backend/Database/Tables/Tag.cs
@@ -2,19 +2,10 @@ using System;
 using System.Data;
 
 namespace Backend.Database.Tables {
-    public class Room : ITable
+    public class Tag : ITable
 {
     public string Id { get; set; }
     public string Title { get; set; }
-    public string Description {get; set;}
-    public int Doors {get; set;}
-    public int Rarity { get; set; }
-    public int Difficulty {get; set;}
-    public string EffectId {get; set;}
-    public Effect Effect {get; set;}
-    public List<string> TagIds {get; set;}
-    public ICollection<Tag> Tags {get; set;}
-
 
     public List<string> GetCollumns()
     {
