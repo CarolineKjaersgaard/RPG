37c85c20b62b7e57a7fdd0af0b539f12eab3d285
CarolineKjaersgaard <caro.thorslund@gmail.com>
2025-11-27 14:22:49 +0100

    adding

--- Backend/Database/Tables/Effect.cs
+++ Backend/Database/Tables/Effect.cs
@@ -4,16 +4,16 @@ using System.Data;
 namespace Backend.Database.Tables {
     public class Effect : ITable
 {
-    public string Id { get; set; }
-    public string Title { get; set; }
-    public string Description {get; set;}
+    public required string Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description {get; set;}
     public bool isActive {get; set;}
     public bool isAoe {get; set;}
     public int Amount {get; set;}
-    public string TargetTypeId {get; set;}
-    public TargetType TargetType {get; set;}
-    public string EffectTypeId {get; set;}
-    public EffectType EffectType {get; set;}
+    public required string TargetTypeId {get; set;}
+    public required TargetType TargetType {get; set;}
+    public required string EffectTypeId {get; set;}
+    public required EffectType EffectType {get; set;}
 
 
     public List<string> GetCollumns()
--- Backend/Database/Tables/Item.cs
+++ Backend/Database/Tables/Item.cs
@@ -4,17 +4,17 @@ using System.Data;
 namespace Backend.Database.Tables {
     public class Item : ITable
 {
-    public string Id { get; set; }
-    public string Title { get; set; }
-    public string Description {get; set;}
+    public required string Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description {get; set;}
     public int Rarity { get; set; }
     public bool isLootable {get; set;}
-    public string EffectId {get; set;}
-    public Effect Effect {get; set;}
-    public string ItemTypeId {get; set;}
-    public ItemType ItemType {get; set;}
-    public List<string> TagIds {get; set;}
-    public ICollection<Tag> Tags {get; set;}
+    public string? EffectId {get; set;}
+    public Effect? Effect {get; set;}
+    public required string ItemTypeId {get; set;}
+    public required ItemType ItemType {get; set;}
+    public List<string>? TagIds {get; set;}
+    public ICollection<Tag>? Tags {get; set;}
 
 
     public List<string> GetCollumns()
