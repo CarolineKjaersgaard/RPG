5debb8a721456851bbc60be9cb90ff807072d40e
CarolineKjaersgaard <caro.thorslund@gmail.com>
2025-11-28 13:46:33 +0100

    connections added

---
+++ Backend/Database/Tables/Connections/LootInRoom.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Data;
+
+namespace Backend.Database.Tables {
+    public class LootInRoom : ITable
+{
+    public required string Id { get; set; }
+    public required string ItemId { get; set; }
+    public required Item Item {get; set;}
+    public required string RoomId { get; set; }
+    public required Room Room {get; set;}
+
+}
+}
