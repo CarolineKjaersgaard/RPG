26a4038839b762b4aff247cfffdabb767a5e8973
CarolineKjaersgaard <caro.thorslund@gmail.com>
2025-12-01 12:21:24 +0100

    more

--- Backend/Database/IDatabase.cs
+++ Backend/Database/IDatabase.cs
@@ -1,10 +1,13 @@
 using Backend.Database.Tables;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Backend.Database
 {
     public interface IDatabase
     {
         public Table GetItem<Table>(string id) where Table: class, ITable;
+
+        public List<Table> GetItems<Table>() where Table: class, ITable;
 
         public List<Table> GetItems<Table>(object value, string column) where Table: class, ITable;
 
